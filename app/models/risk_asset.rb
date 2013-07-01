48493ca8c47d8d062dae4c75c55fc90b5a2fe98b
Erlend Oftedal <erlend@oftedal.no>
2013-07-01 15:40:41 +0200

    Paper trail on assets. CHangelog for assets

--- app/models/risk_asset.rb
+++ app/models/risk_asset.rb
@@ -3,4 +3,20 @@ class RiskAsset < ActiveRecord::Base
   belongs_to :project
   has_and_belongs_to_many :risks
   attr_accessible :description, :name, :risk_asset_value_id
+  has_paper_trail
+
+  def self.to_change_string(version)
+    if (version.event == "create") then
+      c = RiskAsset.find(version.item_id)
+      s = "New risk asset for project #{c.project.id}: <ul>" + 
+        "<li>Name: <em>" + ERB::Util.html_escape(c.name) + "</em></li>" + 
+      	"<li>Value: <em>" + ERB::Util.html_escape(c.risk_asset_value.name) + "</em></li>" +
+      	"<li>Description: <em>" + ERB::Util.html_escape(c.description) + "</em></li>" +
+      	"</ul>"
+      s.html_safe
+    else
+      version.changeset_as_html
+    end
+  end
+
 end
--- app/models/risk_asset_value.rb
+++ app/models/risk_asset_value.rb
@@ -1,4 +1,19 @@
 class RiskAssetValue < ActiveRecord::Base
   belongs_to :risk_configuration
   attr_accessible :name, :weight, :description
+  has_paper_trail
+
+  def self.to_change_string(version)
+    if (version.event == "create") then
+      c = RiskAssetValue.find(version.item_id)
+      s = "New risk asset value for configuration #{c.risk_configuration.id}: <ul>" + 
+        "<li>Name: <em>" + ERB::Util.html_escape(c.name) + "</em></li>" + 
+      	"<li>Weight: <em>#{c.weight}</em></li>" +
+      	"<li>Description: <em>" + ERB::Util.html_escape(c.description) + "</em></li>" +
+      	"</ul>"
+      s.html_safe
+    else
+      version.changeset_as_html
+    end
+  end
 end
