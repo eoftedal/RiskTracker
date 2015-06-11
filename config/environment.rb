43a8a08eaa547901a8d3d6af1e9864c4076e828d
Stian Mathiassen <stian.mathiassen@bekk.no>
2015-06-11 13:25:54 +0200

    Use omniauth for authentication, and upgrade Google authentication to use OAuth 2.0 instead of deprecated OpenID 2.0

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Riskmanager::Application.initialize!
-
-require 'openid'
-OpenID.fetcher = OpenID.fetcher_use_env_http_proxy
-#OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt" 
-OpenID.fetcher.ca_file = ENV['SSL_CERT_FILE']
